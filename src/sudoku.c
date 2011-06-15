12d54418a9b2be2c02b2e462035cec16439365fb
Jon Coppeard <jon@coppeard.net>
2011-06-15 23:07:10 +0100

    Worked on tests, now solves sudoku

--- src/sudoku.c
+++ src/sudoku.c
@@ -27,7 +27,7 @@ typedef digit grid[digit_count];
 typedef unsigned short int digit_set;
 assert_compile(sizeof(digit_set) == 2);
 
-const digit_set all_digits = (1 << grid_size) - 1;
+const digit_set all_digits = (1 << (grid_size + 1)) - 2;
 
 typedef enum
 	{
@@ -47,32 +47,48 @@ int col_from_index(int i)
 	return i % grid_size;
 	}
 
-int index_from_coords(int row, int col)
+int index_from_coords(unsigned int row, unsigned int col)
 	{
+	assert(row < grid_size);
+	assert(col < grid_size);
 	return row * grid_size + col;
 	}
 
-digit_set row_digits_used(grid g, int row)
+#define FOR_ROW_INDEX(row, i)                        \
+	const int row_start = index_from_coords(row, 0); \
+	const int row_end = row_start + grid_size;       \
+	for (int i = row_start ; i < row_end ; ++i)
+
+digit_set row_digits_used(grid g, unsigned int row)
 	{
-	const int start = row * grid_size;
-	const int end = start + grid_size;
 	digit_set result = 0;
-	for (int i = start ; i < end ; ++i)
-		result |= 1 << g[i];
+	FOR_ROW_INDEX(row, i)
+		{
+		digit d = g[i];
+		assert(d <= grid_size);
+		result |= 1 << d;
+		}
 	return result & ~1;
 	}
 
-digit_set col_digits_used(grid g, int col)
+#define FOR_COL_INDEX(col, i)                              \
+	const int col_start = index_from_coords(0, col);       \
+	const int col_end = col_start + digit_count;           \
+	for (int i = col_start ; i < col_end ; i += grid_size)
+
+digit_set col_digits_used(grid g, unsigned int col)
 	{
-	const int start = col;
-	const int end = start + digit_count;
 	digit_set result = 0;
-	for (int i = start ; i < end ; i += grid_size)
-		result |= 1 << g[i];
+	FOR_COL_INDEX(col, i)
+		{
+		digit d = g[i];
+		assert(d <= grid_size);
+		result |= 1 << d;
+		}
 	return result & ~1;
 	}
 
-digit_set square_digits_used(grid g, int row, int col)
+digit_set square_digits_used(grid g, unsigned int row, unsigned int col)
 	{
 	row -= row % square_size;
 	col -= col % square_size;
@@ -143,6 +159,49 @@ bool solve(grid g)
 	return false;
 	}
 
+bool valid_counts(int counts[grid_size + 1])
+	{
+	for (int i = 1 ; i <= grid_size ; ++i)
+		{
+		if (counts[i] != 1)
+			return false;
+		}
+	return true;
+	}
+
+bool valid_solution(grid g)
+	{
+	int counts[grid_size + 1];
+	for (int i = 0 ; i < grid_size ; ++i)
+		{
+		memset(counts, 0, sizeof(counts));
+		FOR_ROW_INDEX(i, j)
+			{
+			digit d = g[j];
+			if (d == 0 || d > grid_size)
+				return false;
+			++counts[d];
+			}
+		if (!valid_counts(counts))
+			return false;
+		
+		memset(counts, 0, sizeof(counts));		
+		FOR_COL_INDEX(i, j)
+			{
+			digit d = g[j];
+			if (d == 0 || d > grid_size)
+				return false;
+			++counts[d];
+			}
+		if (!valid_counts(counts))
+			return false;
+
+		// todo: square
+		
+		}
+	return true;
+	}
+
 err parse_input(const char* input, grid output)
 	{
 	assert_equal(digit_count, strlen(input));
@@ -173,6 +232,24 @@ void format_output(grid input, char* output)
 	output[digit_count] = '\0';
 	}
 
+err parse_and_solve(const char* input)
+	{
+	grid g;
+	err r = parse_input(input, g);
+	if (r != ERR_NONE)
+		return r;
+	if (solve(g))
+		{
+		assert(valid_solution(g));
+		char buffer[grid_size + 1];
+		format_output(g, buffer);
+		printf("%s\n", buffer);
+		}
+	else
+		printf("failed to solve input\n");
+	return ERR_NONE;
+	}
+
 err run_solver()
 	{
 	const int buffer_size = digit_count + 3;  // data + extra to detect overrun + newline + zero
@@ -187,18 +264,9 @@ err run_solver()
 			return ERR_BAD_LINE_LENGTH;
 		assert_equal('\n', buffer[digit_count]);
 		buffer[digit_count] = '\0';
-		
-		grid g;
-		err r = parse_input(buffer, g);
+		err r = parse_and_solve(buffer);
 		if (r != ERR_NONE)
 			return r;
-		if (solve(g))
-			{
-			format_output(g, buffer);
-			printf("%s\n", buffer);
-			}
-		else
-			printf("failed to solve input\n");
 		}
 	return ERR_NONE;
 	}
@@ -247,10 +315,47 @@ err run_tests()
 		assert_equal(i, digit_set_size(d));
 		}
 	
+	printf("  test calculating digit sets for row/column/square\n");
+
 	memset(g, 0, sizeof(g));
 	assert_equal(0, row_digits_used(g, 0));
 	assert_equal(0, col_digits_used(g, 0));
 	assert_equal(0, square_digits_used(g, 0, 0));
+
+	for (int i = 0 ; i < grid_size ; ++i)
+		g[index_from_coords(i, 0)] = i + 1;
+	for (int i = 0 ; i < grid_size ; ++i)
+		{
+		assert_equal(1 << (i + 1), row_digits_used(g, i));
+		assert_equal(i == 0 ? all_digits : 0, col_digits_used(g, i));
+
+		int sc = 3 * (i % square_size);
+		int sr = 3 * (i / square_size);
+		int es = sc == 0 ? (14 << sr) : 0;
+		assert_equal(es, square_digits_used(g, sr, sc));
+		}
+
+	memset(g, 0, sizeof(g));
+	for (int i = 0 ; i < grid_size ; ++i)
+		g[index_from_coords(0, i)] = i + 1;
+	for (int i = 0 ; i < grid_size ; ++i)
+		{
+		assert_equal(i == 0 ? all_digits : 0, row_digits_used(g, i));
+		assert_equal(1 << (i + 1), col_digits_used(g, i));
+		
+		int sc = 3 * (i % square_size);
+		int sr = 3 * (i / square_size);
+		int es = sr == 0 ? (14 << sc) : 0;
+		assert_equal(es, square_digits_used(g, sr, sc));
+		}
+
+	printf("  test solving sudoku\n");
+	const char* i1 = ".94...13..............76..2.8..1.....32.........2...6.....5.4.......8..7..63.4..8";
+	const char* i2 = "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......";
+	const char* i3 = ".................................................................................";
+	assert_equal(ERR_NONE, parse_and_solve(i1));
+	assert_equal(ERR_NONE, parse_and_solve(i2));
+	assert_equal(ERR_NONE, parse_and_solve(i3));
 	
 	printf("All tests passed\n");
 	return ERR_NONE;
