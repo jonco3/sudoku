3e37ad91ac4b9f7246ffdb88a9d0afc29b8ec015
Jon Coppeard <jon@coppeard.net>
2011-06-25 18:30:35 +0100

    Add option to solve from file

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 
 // directly include sudoku solver
 #include "sudoku.c"
+#include "ctype.h"
 
 err run_solver()
 	{
@@ -28,12 +29,61 @@ err run_solver()
 	return ERR_NONE;
 	}
 
+err solve_puzzles_from_file(const char* filename)
+	{
+	const int buffer_size = 256;
+	
+	FILE* file = fopen(filename, "r");
+	if (!file)
+		die("Can't open file %s", filename);
+	int line = 0;
+	
+	for (;;)
+		{
+		char buffer[buffer_size];
+		char* c = fgets(buffer, buffer_size, file);
+		if (!c)
+			{
+			if (feof(file))
+				return ERR_NONE;
+			else
+				die("Error reading file %s", filename);
+			}
+		++line;
+
+		// strip training whitespace
+		for (int i = strlen(buffer) - 1 ; i >= 0 ; --i)
+			{
+			if (isspace((int)buffer[i]))
+				buffer[i] = '\0';
+			}
+
+		// skip empty lines and comments
+		if (buffer[0] == '\0' || buffer[0] == '#')
+			continue;
+		
+		if (strlen(buffer) != digit_count)
+			die("line %d: bad line length");
+
+		err r = parse_and_solve(buffer);
+		if (r != ERR_NONE)
+			return r;
+		}
+	
+	fclose(file);
+	return ERR_NONE;
+	}
+
 int main(int argc, char* argv[])
 	{
-	if (argc != 1)
-		die("usage: soduku [-t]");
+	err r = ERR_NONE;
+	if (argc == 1)
+		r = run_solver();
+	else if (argc == 2)
+		r = solve_puzzles_from_file(argv[1]);
+	else
+		die("usage: soduku [ FILE ]");
 	
-	err r = run_solver();
 	switch (r)
 		{
 		case ERR_NONE:
